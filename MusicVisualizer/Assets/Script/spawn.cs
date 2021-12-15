95e495e1bfdc484e285cff904c2ed4601ec1f071
Yonghui <c15335771@mydit.ie>
2021-12-15 05:19:38 +0000

    edit script

--- MusicVisualizer/Assets/Script/spawn.cs
+++ MusicVisualizer/Assets/Script/spawn.cs
@@ -7,12 +7,13 @@ public class spawn : MonoBehaviour
     public GameObject prefab;
     public static GameObject[] prefabCircle = new GameObject[MusicVis.numSpectrum];
     public static GameObject[] prefabLine = new GameObject[MusicVis.numBand];
-    
+    public float prefabScale = 10;
+    public float smoothSpeed = 10;
 
     // Start is called before the first frame update
     void Start()
     {
-        //SpawnCircle();
+        SpawnCircle();
         SpawnLine();
 
     }
@@ -20,8 +21,9 @@ public class spawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        UpdateCircle();
+        UpdataLine();
 
-        UpdateViz.Update();
     }
 
     void SpawnCircle()
@@ -51,5 +53,29 @@ public class spawn : MonoBehaviour
             line.transform.position = Vector3.right * i * 1.1f;
             prefabLine[i] = line;
         }
+    }
+    void UpdataLine()
+    {
+        for (int i = 0; i < MusicVis.numBand; i++)
+        {
+            if (prefabLine != null)
+            {
+                prefabLine[i].transform.localScale = new Vector3(1, (MusicVis.freqBand[i] * prefabScale), 1);
+            }
+            
+        }
+    }
+
+    void UpdateCircle()
+    {
+        for (int i = 0; i < MusicVis.numSpectrum; i++)
+        {
+            if (prefabCircle != null)
+            {
+                prefabCircle[i].transform.localScale = new Vector3(1, (MusicVis.spectrum[i] * prefabScale), 1);
+            }
+            
+        }
+
     }
 }
